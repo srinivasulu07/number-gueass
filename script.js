97d0039d858112dcd0bd872ab588480157b393fd
srinivasulu07 <56213536+srinivasulu07@users.noreply.github.com>
2021-07-01 15:17:02 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//This will generate a random number 0-9
+const generateTarget = number => {
+    return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+    if (Math.abs(currentHumanGuess - target) < Math.abs(computerGuess - target)) {
+        return true;
+    } else if (Math.abs(computerGuess - target) < Math.abs(currentHumanGuess - target)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    } else {
+        return humanScore += 1;
+    }
+};
+
+const advanceRound = () => {
+    currentRoundNumber += 1;
+}
